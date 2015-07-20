4a0120aa41b22f24f975da55532ee9a69b542b6c
hanshengzhao <hanshengzhao@vip.qq.com>
2015-07-20 12:04:38 +0800

    2

--- kickstart/install.sh
+++ kickstart/install.sh
@@ -1,6 +1,16 @@
 #This is a script
 /bin/rpm -q tftp 
 tftp_install_status=$?
+/bin/rpm -q tftp-server
+tftp_server_install_status=$?
+/bin/rpm -q vsftpd
+vsftpd_install_status=$?
+/bin/rpm -q syslinux 
+syslinux_install_status=$?
+/bin/rpm -q nfs-utils
+nfs_install_status=$?
+
+
 /bin/rpm -q dhcp
 dhcp_install_status=$?
 /bin/rpm -q system-config-kickstart
@@ -9,7 +19,7 @@ kickstart_install_status=$?
 /usr/bin/yum search tftp | grep tftp
 yum_status=$?
 if [ $yum_status -ne 0  ];then
-	if [[ $tftp_install_status -ne 0  ]]&& [[ $dhcp_install_status -ne 0   ]] &&[[ $kickstart_install_status -ne 0  ]] ;then
+	if [[ $tftp_install_status -ne 0  ]]&& [[ $dhcp_install_status -ne 0   ]] &&[[ $kickstart_install_status -ne 0  ]] && [[ $tftp_server_install_status -ne 0  ]] && [[ $vsftpd_install_status -ne 0  ]] && [[ $syslinux_install_status -ne 0 ]] && [[ $nfs_install_status -ne 0 ]]  ;then
 	echo "sorry ,yum is not ok and the program must needed is required"
 	exit 1
 	else 
@@ -18,10 +28,16 @@ if [ $yum_status -ne 0  ];then
 
 else
 	echo "installing the program"
-	/usr/bin/yum  install -y  tftp dhcp system-config-kickstart
+	/usr/bin/yum  install -y  tftp dhcp system-config-kickstart tftp-server vsftpd syslinux nfs-utils
 fi
 
 echo -e  "\e[31m Begining file config.....  \e[0m "
+sed -i 's/disable= yes/disable= no/' /etc/xinetd.d/tftp
+cat > /etc/dhcp/dhcpd.conf <<EOF
 
+
+
+
+EOF
 
 
