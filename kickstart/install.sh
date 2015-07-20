6201d886fe23f9fe12397698ccfcf0e59bf75350
hanshengzhao <hanshengzhao@vip.qq.com>
2015-07-20 16:27:54 +0800

    it worked

--- kickstart/install.sh
+++ kickstart/install.sh
@@ -1,4 +1,5 @@
 #This is a script
+check_rpm(){
 /bin/rpm -q tftp 
 tftp_install_status=$?
 /bin/rpm -q tftp-server
@@ -9,30 +10,38 @@ vsftpd_install_status=$?
 syslinux_install_status=$?
 /bin/rpm -q nfs-utils
 nfs_install_status=$?
-
-
 /bin/rpm -q dhcp
 dhcp_install_status=$?
 /bin/rpm -q system-config-kickstart
 kickstart_install_status=$?
-
 /usr/bin/yum search tftp | grep tftp
 yum_status=$?
+
+iptables -F 
+setenforce 0
+}
+check_rpm >/dev/null 2>&1
+
 if [ $yum_status -ne 0  ];then
 	if [[ $tftp_install_status -ne 0  ]]&& [[ $dhcp_install_status -ne 0   ]] &&[[ $kickstart_install_status -ne 0  ]] && [[ $tftp_server_install_status -ne 0  ]] && [[ $vsftpd_install_status -ne 0  ]] && [[ $syslinux_install_status -ne 0 ]] && [[ $nfs_install_status -ne 0 ]]  ;then
 	echo "sorry ,yum is not ok and the program must needed is required"
 	exit 1
 	else 
-	echo "you have installed the program "
+	echo " ?"
+	/usr/bin/yum  install -y  tftp dhcp system-config-kickstart tftp-server vsftpd syslinux nfs-utils
 	fi
 
 else
+	if [[ $tftp_install_status -ne 0  ]]&& [[ $dhcp_install_status -ne 0   ]] &&[[ $kickstart_install_status -ne 0  ]] && [[ $tftp_server_install_status -ne 0  ]] && [[ $vsftpd_install_status -ne 0  ]] && [[ $syslinux_install_status -ne 0 ]] && [[ $nfs_install_status -ne 0 ]]  ;then
 	echo "installing the program"
 	/usr/bin/yum  install -y  tftp dhcp system-config-kickstart tftp-server vsftpd syslinux nfs-utils
+	else
+	echo -e  "\e[32m Everything is Ok...  \e[0m"
+	fi
 fi
 
-echo -e  "\e[31m Begining file config.....  \e[0m "
-sed -i 's/disable= yes/disable= no/' /etc/xinetd.d/tftp
+echo -e  "\e[32m Begining file config.....  \e[0m "
+sed -i '14 s/yes/no/' /etc/xinetd.d/tftp
 IP=`ifconfig   |grep -v 127| grep "inet addr" | awk '{print $2}'  | awk -F ":" '{print $2}'`
 IP_NET=`echo $IP | awk -F '.' '{print $1"."$2"."$3}'`
 
@@ -56,50 +65,71 @@ next-server $IP;
 }
 EOF
 
-cp /usr/shard/syslinux/pxelinux.0 /var/lib/tftpboot
-mount  /dev/cdrom  /media/cdrom
-cp /media/cdrom/isolinux/*  /var/lib/tftpboot/
-cd /var/lib/tftpboot/;mkdir pxelinux.cfg;cd pxelinux.cfg
+cp /usr/share/syslinux/pxelinux.0 /var/lib/tftpboot
+mount  /dev/cdrom  /media  >/dev/null 2>&1
+cp /media/isolinux/*  /var/lib/tftpboot/
+cd /var/lib/tftpboot/;
+if [ ! -d "pxelinux.cfg" ];then
+mkdir pxelinux.cfg
+fi
+cd pxelinux.cfg
 cat >default<<EOF
-    default linux
-    #default vesamenu.c32
-    prompt 1
-    timeout 600
-     
-    display boot.msg
-     
-    menu background splash.jpg
-    menu title Welcome to CentOS 6.5!
-    menu color border 0 #ffffffff #00000000
-    menu color sel 7 #ffffffff #ff000000
-    menu color title 0 #ffffffff #00000000
-    menu color tabmsg 0 #ffffffff #00000000
-    menu color unsel 0 #ffffffff #00000000
-    menu color hotsel 0 #ff000000 #ffffffff
-    menu color hotkey 7 #ffffffff #ff000000
-    menu color scrollbar 0 #ffffffff #00000000
-     
-    label linux
-    menu label ^Install or upgrade an existing system
-    menu default
-    kernel vmlinuz
-    append initrd=initrd.img ks=ftp://192.168.1.101/pub/ks.cfg
-    label vesa
-    menu label Install system with ^basic video driver
-    kernel vmlinuz
-    append initrd=initrd.img xdriver=vesa nomodeset
-    label rescue
-    menu label ^Rescue installed system
-    kernel vmlinuz
-    append initrd=initrd.img rescue
-    label local
-    menu label Boot from ^local drive
-    localboot 0xffff
-    label memtest86
-    menu label ^Memory test
-    kernel memtest
-    append -
+default linux
+prompt 1
+timeout 10
+display boot.msg
+menu background splash.jpg
+menu title www.7moor.com
+menu color border 0 #ffffffff #00000000
+menu color sel 7 #ffffffff #ff000000
+menu color title 0 #ffffffff #00000000
+menu color tabmsg 0 #ffffffff #00000000
+menu color unsel 0 #ffffffff #00000000
+menu color hotsel 0 #ff000000 #ffffffff
+menu color hotkey 7 #ffffffff #ff000000
+menu color scrollbar 0 #ffffffff #00000000
+ 
+label linux
+menu label ^Install 7moor's system
+menu default
+kernel vmlinuz
+append initrd=initrd.img ks=ftp://$IP/pub/ks.cfg
+
+label local
+menu label Boot from ^local drive
+localboot 0xffff
 
 EOF
+
+cat >/etc/exports <<EOF
+/media *(ro,sync)
+EOF
+
+start_server(){
+/etc/init.d/rpcbind restart  
+/etc/init.d/nfs restart 
+/etc/init.d/tftp restart
+/etc/init.d/dhcpd restart
+/etc/init.d/vsftpd restart
+
+
+} 
+
+
+start_server >/dev/null 2>&1
+showmount -e localhost
+
+sed -i 's/server=[^a-z]*/server='''$IP''' --/' /var/ftp/pub/ks.cfg 
+
+
+
+
+
+
+
+
+
+
+
 
 
